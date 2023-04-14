013d31bdd7b1f4c7411c84ec4124b0f45672121c
Krzysztof Kalus <eumo37@gmail.com>
2023-04-14 16:03:41 +0200

    function added

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
-console.log("Cześć! Miło mi, że zaglądasz tutaj.")
-
-let changeButton = document.querySelector(".footer__button");
-let homeAdres = document.querySelector(".footer__email");
-
-changeButton.addEventListener("click", () => {
-    if (changeButton.innerText === "Pokaż_adres_hodowli") {
-        homeAdres.innerText = "Chorzów, Rynek 2";
-        changeButton.innerText = "Pokaż_adres_e-mail";
-    } else {
-        homeAdres.innerText = "k.kalus@euco.pl";
-        changeButton.innerText = "Pokaż_adres_hodowli";
+{
+    const welcome = () => {
+        console.log("Cześć! Miło mi, że zaglądasz tutaj.")
     }
-});
+
+    const onChangeBacgroundClick = () => {
+        const changeButton = document.querySelector(".footer__button");
+        const homeAdres = document.querySelector(".footer__email");
+        if (changeButton.innerText === "Pokaż_adres_hodowli") {
+            homeAdres.innerText = "Chorzów, Rynek 2";
+            changeButton.innerText = "Pokaż_adres_e-mail";
+        } else {
+            homeAdres.innerText = "k.kalus@euco.pl";
+            changeButton.innerText = "Pokaż_adres_hodowli";
+        }
+    }
+
+    const init = () => {
+        const changeButton = document.querySelector(".footer__button");
+        changeButton.addEventListener("click", onChangeBacgroundClick);
+        welcome();
+    }
+    init();
+}
